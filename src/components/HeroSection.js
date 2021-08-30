60e92558e0a0153d3c453cd6a5a6cf95fa2b4cd0
Caner Uçar <caner.ucar@akinon.com>
2021-08-30 21:04:09 +0300

    Add HeroSection component content

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {FaFacebook} from 'react-icons/fa';
+import Slider from 'react-slick';
+import ReactFlagsSelect from 'react-flags-select';
 
 function HeroSection() {
-  return <div>hero section</div>;
+
+  const [selected, setSelected] = useState('TR');
+
+  const phones = {
+    US: '+1',
+    DE: '+50',
+    TR: '+90',
+    IT: '+45',
+    IN: '+12'
+  }
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 3500,
+    cssEase: "linear"
+  };
+
+  return (
+    <div className="relative h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
+      <Slider {...settings}>
+        <div>
+          <img
+            className="w-full h-[500px] object-cover"
+            src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg"
+            alt="getir images"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-[500px] object-cover"
+            src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg"
+            alt="getir images"
+          />
+        </div>
+      </Slider>
+
+      <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
+        <div>
+          <img
+            src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg"
+            alt="getir icon"
+          />
+          <h3 className="mt-5 text-4xl font-semibold text-white">
+            Dakikalar içinde <br /> kapınızda
+          </h3>
+        </div>
+
+        <div className="w-[400px] rounded-lg bg-gray-50 p-6">
+          <h4 className="text-primary-brand-color text-center font-semibold text-sm mb-4">
+            Giriş yap veya kayıt ol
+          </h4>
+          <div className="grid gap-y-3">
+            <div className="flex gap-x-2">
+              <ReactFlagsSelect
+                countries={Object.keys(phones)}
+                customLabels={phones}
+                selected={selected}
+                onSelect={(code) => setSelected(code)}
+                className="flag-select"
+              />
+              <label className="flex-1 group relative block cursor-pointer">
+                <input
+                  required
+                  className="h-14 px-4 border-2 border-gray-200 rounded w-full transition-colors group-hover:border-primary-brand-color focus:border-primary-brand-color outline-none peer text-sm pt-2"
+                />
+                <span className="absolute top-0 left-0 h-full px-4 flex items-center text-sm text-gray-700 transition-all peer-focus:h-5 peer-valid:text-primary-brand-color peer-valid:text-xs peer-valid:h-5 peer-focus:mt-1 peer-valid:mt-1">
+                  Telefon Numarası
+                </span>
+              </label>
+            </div>
+            <button className="bg-brand-yellow text-primary-brand-color transition-colors hover:bg-primary-brand-color hover:text-brand-yellow h-12 flex items-center justify-center rounded-md w-full text-sm font-semibold">
+              Telefon numarası ile devam et
+            </button>
+            <hr className="h-[1px] bg-gray-300 my-2" />
+            <button className="bg-blue-700 bg-opacity-10 px-4 text-blue-700 text-opacity-80 transition-colors hover:bg-blue-700 hover:text-white h-12 flex items-center rounded-md w-full text-sm font-semibold">
+              <FaFacebook size={24} />
+              <span className="mx-auto">Facebook ile devam et</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundColor: theme => ({
-        'brand-color': '#5d3ebc'
-      })
+      colors: {
+        'brand-color': '#4c3398',
+        'primary-brand-color': '#5d3ebc',
+        'secondary-brand-color': '#7849f7',
+        'brand-yellow': '#ffd300'
+      }
     },
   },
   variants: {
