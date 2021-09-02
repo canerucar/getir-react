472a8cfa00dd15ff11085c4f57baa99410e9d0fd
Caner Uçar <caner.ucar@akinon.com>
2021-09-02 18:55:46 +0300

    Add MobileApp, Cards, Footer components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import cardsData from 'api/cards';
 
 function Cards() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    setCards(cardsData);
+  }, []);
   return (
     <div>
-      cards
+      <div className="grid grid-cols-3 gap-x-4">
+        {cards.length &&
+          cards.map((card) => (
+            <div className="bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center">
+              <img className="w-[150px] h-[150px] mb-6" src={card.image} alt={card.title} />
+              <h6 className="font-semibold text-lg text-primary-brand-color">
+                {card.title}
+              </h6>
+              <p className="mt-2 text-sm text-gray-700">{card.description}</p>
+            </div>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Cards
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,18 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        '0.1': '0.063rem'
+        0.1: '0.063rem',
       },
+      backgroundImage: (theme) => ({
+        'mobile-app':
+          'url(https://getir.com/_next/static/images/doodle-d659f9f1fd505c811c2331fe3ffddd5f.png)',
+      }),
       colors: {
         'brand-color': '#4c3398',
         'primary-brand-color': '#5d3ebc',
         'secondary-brand-color': '#7849f7',
-        'brand-yellow': '#ffd300'
-      }
+        'brand-yellow': '#ffd300',
+      },
     },
   },
   variants: {
